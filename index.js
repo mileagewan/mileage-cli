391ad044d1dc6305e54ab315e3be6aab6ac0d5c7
mileage <792668520@qq.com>
2022-03-20 18:25:14 +0800

    【story开发/问题单号】：新开发 【作者】：wlc 【修改详情】：发布自己的脚手架

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const program = require('commander');
 const download = require('download-git-repo');
 //version 版本号
 //name 新项目名称
-program.version('1.0.0', '-v, --version')
+program.version('1.0.2', '-v, --version')
   .command('init <templateName> <projectName>')
   .action((templateName, projectName) => {
     if (templateName === "vue") {
